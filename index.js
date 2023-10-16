53c4fc9caf49e2674628a03a44a92befb90e696c
Jared Patterson <jpwesley34@gmail.com>
2023-10-15 17:20:27 -0700

    clicks stuff

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    function clickAlert() {
+        alert('Stop clicking me!');
+    }
+    input.addEventListener('click', clickAlert);
 }
+
+addingEventListener();
